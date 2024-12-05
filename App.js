6b3f6405328b7ce95b21533765991e991fffe494
menescalnickolas <menescalnickolas@gmail.com>
2024-12-05 19:07:54 +0100

    Fixed App initialization to prevent duplicate

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
-
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Start from './components/Start';
 import Chat from './components/Chat';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { initializeApp } from "firebase/app";
 import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNetInfo } from '@react-native-community/netinfo';
 import { useEffect } from "react";
 import { LogBox, Alert } from "react-native";
 import { getStorage } from "firebase/storage";
+ import { getApps, initializeApp } from "firebase/app";
+
 
 const Stack = createNativeStackNavigator();
 
@@ -38,8 +36,13 @@ export default function App() {
     appId: "1:257547840215:web:d6ee521d480f1d57221e92"
   };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+ //Initialize firebase
+  let app;
+  if (!getApps().length) {
+    app = initializeApp(firebaseConfig);
+  } else {
+    app = getApps()[0]; // Use the already initialized app
+  }
 
   // Initialize Cloud Firestore and get a reference to the service
   const db = getFirestore(app);
@@ -72,92 +75,3 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
-
-
-
-
-/*
-
-//Import the screens
-import Start from './components/Start';
-import Chat from './components/Chat';
-
-//Import react Navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-//Create the navigator
-const Stack = createNativeStackNavigator();
-
-import { initializeApp } from "firebase/app";
-import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-
-import { useNetInfo } from "@react-native-community/netinfo";
-import { useEffect } from "react";
-import { LogBox, Alert } from "react-native";
-
-
-
-
-
-const App = () => {
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyC2eBC3LneODX9I8xw-_tHFOlRBPwcjaOI",
-    authDomain: "chat-bde4b.firebaseapp.com",
-    projectId: "chat-bde4b",
-    storageBucket: "chat-bde4b.firebasestorage.app",
-    messagingSenderId: "257547840215",
-    appId: "1:257547840215:web:d6ee521d480f1d57221e92"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
-  // Initialize Cloud Firestore and get a reference to the service
-  const db = getFirestore(app);
-
-  //Initialize storage handler
-  const storage = getStorage(app);
-
-  //Define a new state that represents the network connectivity status
-  const connectionStatus = useNetInfo();
-
-  //useEffect() code that will display an alert popup if connection is lost
-  useEffect(() => {
-    if (connectionStatus.isConnected === false) {
-      Alert.alert("Connection Lost!");
-      disableNetwork(db);
-    } else if (connectionStatus.isConnected === true) {
-      enableNetwork(db);
-    }
-  }, [connectionStatus.isConnected]);
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Start"
-      >
-        <Stack.Screen
-          name="Start"
-          component={Start}
-        />
-        <Stack.Screen
-          name="Chat">
-          {props => <Chat
-            isConnected={connectionStatus.isConnected}
-            db={db}
-            storage={storage}
-            {...props}
-          />}
-        </Stack.Screen>
-      </Stack.Navigator>
-
-    </NavigationContainer>
-  );
-}
-
-export default App;
-
-*/
